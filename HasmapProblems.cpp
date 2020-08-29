1bfd1aa6cb972e227b7f82b364c992e36650a95e
Ashish Raghuvanshi <thetypo36@gmail.com>
2020-08-29 23:28:38 +0530

    hashmap problems

--- HasmapProblems.cpp
+++ HasmapProblems.cpp
@@ -1,5 +1,6 @@
 #include <unordered_map>
 #include <iostream>
+#include <climits>
 #include <vector>
 using namespace std;
 int highestFreq(int *arr, int n)
@@ -74,15 +75,133 @@ void printPairSumTo0(int *arr, int size)
     for (int i = 0; i < size; i++)
     {
         int p = 0 - arr[i];
-        if(freq[arr[i]]>0&&freq[p]>0){
-            int print = freq[arr[i]]*freq[p];
-            while(print--){
-                cout << p << " " << arr[i]<< endl;
+        if (freq[arr[i]] > 0 && freq[p] > 0)
+        {
+            int print = freq[arr[i]] * freq[p];
+            while (print--)
+            {
+                cout << p << " " << arr[i] << endl;
             }
         }
-        freq[arr[i]]=0;
-        freq[p]=0;
+        freq[arr[i]] = 0;
+        freq[p] = 0;
+    }
+}
+char *uniqueChar(char *str)
+{
+    // Write your code here
 
+    unordered_map<char, int> freq;
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+        freq[str[i]]++;
+    }
+    char *out = new char[freq.size()];
+    int j = 0;
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+        if (freq[str[i]] > 0)
+        {
+            out[j] = str[i];
+            j++;
+        }
+        freq[str[i]] = 0;
+    }
+    out[j] = '\0';
+    return out;
+}
+vector<int> longestConsecutiveIncreasingSequence(int *arr, int n)
+{
+    unordered_map<int, bool> pres;
+    for (int i = 0; i < n; i++)
+    {
+        pres[arr[i]] = true;
+    }
+    int maxL = 0, maxS = arr[0];
+    for (int i = 0; i < n; i++)
+    {
+        int maxC = 0, j = 1;
+        while (pres.count(arr[i] + j) > 0)
+        {
+            maxC++;
+            j++;
+        }
+        if (maxC > maxL)
+        {
+            maxL = maxC;
+            maxS = arr[i];
+        }
+    }
+    vector<int> out;
+    for (int i = 0; i <= maxL; i++)
+    {
+        out.push_back(maxS + i);
+    }
+    return out;
+}
+int LongestSubseqSumTo0(int* arr, int n){
+    unordered_map<int,int> cum;
+    int max = 0;
+    int sum = 0;
+    for(int i = 0 ; i < n; i++){
+        sum+=arr[i];
+        if(cum.count(sum)>0){
+            int cur = i - cum[sum];
+            if(cur > max){
+                max = cur;
+            }
+        }else{
+            cum[sum]=i;
+        }
+    }
+    return max;
+}
+void printPairs(int *input, int n, int k)
+{
+    // Write your code here
+    unordered_map<int, int> freq;
+    if(freq.size()==1)
+    {
+        int print = freq[input[0]]*2;
+        print = print -2;
+        while (print--)
+        {
+        cout << input[0] << " " << input[0] << endl;
+        }
+        
+    }
+    for (int i = 0; i < n; i++)
+    {
+        freq[input[i]]++;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        int after = input[i] - k;
+        int before = k - input[i];
+
+        if (freq.count(after) > 0)
+        {
+            int print = freq[after] * freq[input[i]];
+            while (print--)
+            {
+                if (after <= input[i])
+                    cout << after << " " << input[i] << endl;
+                else
+                    cout << input[i] << " " << after << endl;
+            }
+        }
+        if (freq.count(before) > 0)
+        {
+            int print = freq[before] * freq[input[i]];
+            while (print--)
+            {
+                
+                if (before <= input[i])
+                    cout << before << " " << input[i] << endl;
+                else
+                    cout << input[i] << " " << before << endl;
+            }
+        }
     }
 }
 int main()
@@ -121,7 +240,7 @@ int main()
         cin >> arr2[i];
     }
     cout << "****output****" << endl;
-    printPairSumTo0(arr2,n2);
+    printPairSumTo0(arr2, n2);
     //intersection(arr, arr2, size, n2);
     //delete[] arr;
     delete[] arr2;
