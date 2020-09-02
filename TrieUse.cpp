58fef1bc243faf27993f206d45993843f2332481
Ashish Raghuvanshi <thetypo36@gmail.com>
2020-09-02 22:05:40 +0530

    Tries Implementation

--- TrieUse.cpp
+++ TrieUse.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 int main()
 {
+ 
     Trie t;
    t.insert("ashish");
     t.insert("anand");
@@ -12,8 +13,9 @@ int main()
     cout << t.search("anand") << endl;
     cout << t.search("ayush") << endl;
     cout << "*******************" << endl;
-    remove("ashish");
+    t.remove("ashish");
     cout << t.search("ayush") << endl;
     cout << t.search("ashish") << endl;
     cout << t.search("anand") << endl;
+
 }
