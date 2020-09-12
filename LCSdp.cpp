ea9eb9569b6d51f7f14e3b4b19bedb281169f052
Ashish Raghuvanshi <thetypo36@gmail.com>
2020-09-12 12:49:43 +0530

    2d DP problems

--- LCSdp.cpp
+++ LCSdp.cpp
@@ -2,42 +2,27 @@
 using namespace std;
 int lcsDp(string s1, string s2)
 {
-    int **strg = new int *[s1.length() + 1];
-    for (int i = 0; i <= s1.length(); i++)
-    {
-        strg[i] = new int[s2.length() + 1];
-        for (int j = 0; j <= s2.length(); j++)
-        {
-            strg[i][j] = 0;
-        }
-    }
     int m = s1.length();
     int n = s2.length();
 
-    for (int i = 0; i <= m; i++)
+    vector<vector<int>> strg((m+1),vector<int>(n+1,0)) ;
+    
+    for (int i = 0; i < m+1; i++)
     {
-        strg[i][0] = 0;
-    }
-    for (int i = 0; i <= n; i++)
-    {
-
-        strg[0][i] = 0;
-    }
-
-    for (int i = 1; i <= m; i++)
-    {
-        for (int j = 1; j <= n; j++)
+        for (int j = 0; j < n+1; j++)
         {
-            if (s1[m-i] == s2[n-j])
-            {
-                strg[i][j] = 1 + strg[i - 1][j - 1];
-            }
-            else
-            {
-                strg[i][j] = max(strg[i - 1][j - 1], max(strg[i][j - 1], strg[i - 1][j]));
+           // cout << strg[i][j] <<" ";
+            if(i==0||j==0){
+                strg[i][j]=0;
+            }else if(s1[i-1]==s2[j-1]){
+                strg[i][j] = 1+strg[i-1][j-1];
+            }else{
+                strg[i][j] = max(strg[i-1][j],strg[i][j-1]);
             }
         }
+        //cout << endl;
     }
+    
     return strg[m][n];
 }
 int lcsMemoization(string s1, string s2, int **strg)
--- minCoinChange.cpp
+++ minCoinChange.cpp
@@ -1,56 +1,107 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-int minCoinChangeMemoization(int* coins, int n ,int value,int* strg)
+int minCoinChangeDp(int *coins, int n, int value)
 {
-    if(value == 0){
+    int **strg = new int *[n + 1];
+    for (int i = 0; i < n + 1; i++)
+    {
+        strg[i] = new int[value + 1];
+    }
+    for (int i = 0; i < n + 1; i++)
+    {
+        for (int j = 0; j < value + 1; j++)
+        {
+            if (i == 0)
+            {
+                strg[i][j] = INT_MAX - 1;
+            }
+            if (j == 0)
+            {
+                strg[i][j] = 0;
+            }
+            if (i == 1 && j > 0)
+            {
+                if (j % coins[0] == 0)
+                {
+                    strg[i][j] = j / coins[0];
+                }
+                else
+                {
+                    strg[i][j] = INT_MAX - 1;
+                }
+            }
+        }
+    }
+    for (int i = 2; i < n + 1; i++)
+    {
+        for (int j = 1; j < value + 1; j++)
+        {
+            if (coins[i - 1] > j)
+            {
+                strg[i][j] = strg[i - 1][j];
+            }
+            else
+            {
+                strg[i][j] = min(strg[i - 1][j], 1 + strg[i][j - coins[i - 1]]);
+            }
+        }
+    }
+
+    return strg[n][value];
+}
+int minCoinChangeMemoization(int *coins, int n, int value, int *strg)
+{
+    if (value == 0)
+    {
         return 0;
     }
-    if(strg[value]!=-1){
+    if (strg[value] != -1)
+    {
         return strg[value];
     }
     int result = INT_MAX;
     for (int i = 0; i < n; i++)
     {
-        if(coins[i]<=value){
-            result = min(result,1+minCoinChangeMemoization(coins,n,value - coins[i],strg));
+        if (coins[i] <= value)
+        {
+            result = min(result, 1 + minCoinChangeMemoization(coins, n, value - coins[i], strg));
         }
     }
     return strg[value] = result;
 }
-int minCoinChange(int* coins, int n , int value){
-    if(value == 0)
-    return 0;
+int minCoinChange(int *coins, int n, int value)
+{
+    if (value == 0)
+        return 0;
     int result = INT_MAX;
     for (int i = 0; i < n; i++)
     {
-        if(coins[i]<= value){
-            result = min(result,1+minCoinChange(coins,n,value - coins[i]));
+        if (coins[i] <= value)
+        {
+            result = min(result, 1 + minCoinChange(coins, n, value - coins[i]));
         }
-
     }
     return result;
 }
-int main(){
+int main()
+{
 
     int n;
-   cin >> n;
-    int * coins  = new int[n];
+    cin >> n;
+    int *coins = new int[n];
     for (int i = 0; i < n; i++)
     {
         cin >> coins[i];
     }
     int value;
     cin >> value;
-    int * strg = new int[value];
+    int *strg = new int[value];
 
-    for (int i = 0; i < value+1; i++)
+    for (int i = 0; i < value + 1; i++)
     {
         strg[i] = -1;
     }
-    
-    cout << "Memoization Approach: " << minCoinChangeMemoization(coins,n,value,strg) << endl;
-    cout << "Recursive Approach: " << minCoinChange(coins,n,value) << endl;
-
-    
+    cout << "Dp Approach: " << minCoinChangeDp(coins, n, value) << endl;
+    cout << "Memoization Approach: " << minCoinChangeMemoization(coins, n, value, strg) << endl;
+    cout << "Recursive Approach: " << minCoinChange(coins, n, value) << endl;
 }
