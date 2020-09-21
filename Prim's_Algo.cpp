bca0d56fb95e1be681a52724a52c8a8537f4d710
Ashish Raghuvanshi <thetypo36@gmail.com>
2020-09-21 12:16:31 +0530

    Prim's Algorithm

--- Prim's_Algo.cpp
+++ Prim's_Algo.cpp
@@ -41,7 +41,7 @@ void prims_algo(int **edges, int V, int E)
             }
         }
     }
-    //cout << " ******************** " << endl;
+    cout << endl;
     for (int i = 1; i < V; i++)
     {
         if (i < parent[i])
